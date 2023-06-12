66c9d2af8319e850ee6b60c0b4b4d764d62035a0
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-06-12 20:53:43 +1000

    Headers, whatgpt and features

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+
 import "./features.css";
 
 const Features = () => {
-  return <div>Features</div>;
+  return <h1>Features</h1>;
 };
 
 export default Features;
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+
+import Feature from "../../components/feature/Feature";
 import "./whatGPT3.css";
 
 const WhatGPT3 = () => {
-  return <div>WhatGPT3</div>;
+  return (
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          The possibilities are beyond our imagination
+        </h1>
+        <p>Explore Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
+  );
 };
 
 export default WhatGPT3;
